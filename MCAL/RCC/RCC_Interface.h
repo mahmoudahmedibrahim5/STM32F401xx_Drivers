7320e6705227001c59c616e7238e80980ce71ac2
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2023-02-14 11:29:08 +0200

    Enable is Completed + Added LP enable and Reset

--- MCAL/RCC/RCC_Interface.h
+++ MCAL/RCC/RCC_Interface.h
@@ -11,6 +11,7 @@
 #include "RCC_Registers.h"
 
 typedef enum {
+	// AHB1
 	GPIOA_EN,
 	GPIOB_EN,
 	GPIOC_EN,
@@ -20,9 +21,37 @@ typedef enum {
 	CRC_EN,
 	DMA1_EN,
 	DMA2_EN,
-	OTGFS_EN
-	// to be continued
+	// AHB2
+	OTGFS_EN,
+	// APB1
+	TIM2_EN,
+	TIM3_EN,
+	TIM4_EN,
+	TIM5_EN,
+	WWDG_EN,
+	SPI2_EN,
+	SPI3_EN,
+	USART2_EN,
+	I2C1_EN,
+	I2C2_EN,
+	I2C3_EN,
+	PWR_EN,
+	// APB2
+	TIM1_EN,
+	USART1_EN,
+	USART6_EN,
+	ADC1_EN,
+	SDIO_EN,
+	SPI1_EN,
+	SPI4_EN,
+	SYSCFG_EN,
+	TIM9_EN,
+	TIM10_EN,
+	TIM11_EN
 }En_peripheral_t;
+
 void RCC_peripheralEn(En_peripheral_t per);
+void RCC_peripheralLowPowerEn(En_peripheral_t per);
+void RCC_peripheralReset(En_peripheral_t per);
 
 #endif /* RCC_RCC_INTERFACE_H_ */
--- MCAL/RCC/RCC_Program.c
+++ MCAL/RCC/RCC_Program.c
@@ -12,6 +12,7 @@ void RCC_peripheralEn(En_peripheral_t per)
 {
 	switch(per)
 	{
+	/* AHB1 */
 	case GPIOA_EN:
 			RCC->AHB1ENR |= (1<<0);
 			break;
@@ -30,7 +31,315 @@ void RCC_peripheralEn(En_peripheral_t per)
 	case GPIOH_EN:
 			RCC->AHB1ENR |= (1<<7);
 			break;
+	case CRC_EN:
+			RCC->AHB1ENR |= (1<<12);
+			break;
+	case DMA1_EN:
+			RCC->AHB1ENR |= (1<<21);
+			break;
+	case DMA2_EN:
+			RCC->AHB1ENR |= (1<<22);
+			break;
+	/* AHB2 */
+	case OTGFS_EN:
+			RCC->AHB2ENR |= (1<<7);
+			break;
+	/* APB1 */
+	case TIM2_EN:
+			RCC->APB1ENR |= (1<<0);
+			break;
+	case TIM3_EN:
+			RCC->APB1ENR |= (1<<1);
+			break;
+	case TIM4_EN:
+			RCC->APB1ENR |= (1<<2);
+			break;
+	case TIM5_EN:
+			RCC->APB1ENR |= (1<<3);
+			break;
+	case WWDG_EN:
+			RCC->APB1ENR |= (1<<11);
+			break;
+	case SPI2_EN:
+			RCC->APB1ENR |= (1<<14);
+			break;
+	case SPI3_EN:
+			RCC->APB1ENR |= (1<<15);
+			break;
+	case USART2_EN:
+			RCC->APB1ENR |= (1<<17);
+			break;
+	case I2C1_EN:
+			RCC->APB1ENR |= (1<<21);
+			break;
+	case I2C2_EN:
+			RCC->APB1ENR |= (1<<22);
+			break;
+	case I2C3_EN:
+			RCC->APB1ENR |= (1<<23);
+			break;
+	case PWR_EN:
+			RCC->APB1ENR |= (1<<28);
+			break;
+	/* APB2 */
+	case TIM1_EN:
+			RCC->APB2ENR |= (1<<0);
+			break;
+	case USART1_EN:
+			RCC->APB2ENR |= (1<<4);
+			break;
+	case USART6_EN:
+			RCC->APB2ENR |= (1<<5);
+			break;
+	case ADC1_EN:
+			RCC->APB2ENR |= (1<<8);
+			break;
+	case SDIO_EN:
+			RCC->APB2ENR |= (1<<11);
+			break;
+	case SPI1_EN:
+			RCC->APB2ENR |= (1<<12);
+			break;
+	case SPI4_EN:
+			RCC->APB2ENR |= (1<<13);
+			break;
+	case SYSCFG_EN:
+			RCC->APB2ENR |= (1<<14);
+			break;
+	case TIM9_EN:
+			RCC->APB2ENR |= (1<<16);
+			break;
+	case TIM10_EN:
+			RCC->APB2ENR |= (1<<17);
+			break;
+	case TIM11_EN:
+			RCC->APB2ENR |= (1<<18);
+			break;
 	default:
 		break;
+	}
+}
+
+void RCC_peripheralLowPowerEn(En_peripheral_t per)
+{
+	switch(per)
+		{
+		/* AHB1 */
+		case GPIOA_EN:
+				RCC->AHB1LPENR |= (1<<0);
+				break;
+		case GPIOB_EN:
+				RCC->AHB1LPENR |= (1<<1);
+				break;
+		case GPIOC_EN:
+				RCC->AHB1LPENR |= (1<<2);
+				break;
+		case GPIOD_EN:
+				RCC->AHB1LPENR |= (1<<3);
+				break;
+		case GPIOE_EN:
+				RCC->AHB1LPENR |= (1<<4);
+				break;
+		case GPIOH_EN:
+				RCC->AHB1LPENR |= (1<<7);
+				break;
+		case CRC_EN:
+				RCC->AHB1LPENR |= (1<<12);
+				break;
+		case DMA1_EN:
+				RCC->AHB1LPENR |= (1<<21);
+				break;
+		case DMA2_EN:
+				RCC->AHB1LPENR |= (1<<22);
+				break;
+		/* AHB2 */
+		case OTGFS_EN:
+				RCC->AHB2LPENR |= (1<<7);
+				break;
+		/* APB1 */
+		case TIM2_EN:
+				RCC->APB1LPENR |= (1<<0);
+				break;
+		case TIM3_EN:
+				RCC->APB1LPENR |= (1<<1);
+				break;
+		case TIM4_EN:
+				RCC->APB1LPENR |= (1<<2);
+				break;
+		case TIM5_EN:
+				RCC->APB1LPENR |= (1<<3);
+				break;
+		case WWDG_EN:
+				RCC->APB1LPENR |= (1<<11);
+				break;
+		case SPI2_EN:
+				RCC->APB1LPENR |= (1<<14);
+				break;
+		case SPI3_EN:
+				RCC->APB1LPENR |= (1<<15);
+				break;
+		case USART2_EN:
+				RCC->APB1LPENR |= (1<<17);
+				break;
+		case I2C1_EN:
+				RCC->APB1LPENR |= (1<<21);
+				break;
+		case I2C2_EN:
+				RCC->APB1LPENR |= (1<<22);
+				break;
+		case I2C3_EN:
+				RCC->APB1LPENR |= (1<<23);
+				break;
+		case PWR_EN:
+				RCC->APB1LPENR |= (1<<28);
+				break;
+		/* APB2 */
+		case TIM1_EN:
+				RCC->APB2LPENR |= (1<<0);
+				break;
+		case USART1_EN:
+				RCC->APB2LPENR |= (1<<4);
+				break;
+		case USART6_EN:
+				RCC->APB2LPENR |= (1<<5);
+				break;
+		case ADC1_EN:
+				RCC->APB2LPENR |= (1<<8);
+				break;
+		case SDIO_EN:
+				RCC->APB2LPENR |= (1<<11);
+				break;
+		case SPI1_EN:
+				RCC->APB2LPENR |= (1<<12);
+				break;
+		case SPI4_EN:
+				RCC->APB2LPENR |= (1<<13);
+				break;
+		case SYSCFG_EN:
+				RCC->APB2LPENR |= (1<<14);
+				break;
+		case TIM9_EN:
+				RCC->APB2LPENR |= (1<<16);
+				break;
+		case TIM10_EN:
+				RCC->APB2LPENR |= (1<<17);
+				break;
+		case TIM11_EN:
+				RCC->APB2LPENR |= (1<<18);
+				break;
+		default:
+			break;
+		}
+}
+
+void RCC_peripheralReset(En_peripheral_t per)
+{
+	switch(per)
+	{
+		/* AHB1 */
+		case GPIOA_EN:
+				RCC->AHB1RSTR |= (1<<0);
+				break;
+		case GPIOB_EN:
+				RCC->AHB1RSTR |= (1<<1);
+				break;
+		case GPIOC_EN:
+				RCC->AHB1RSTR |= (1<<2);
+				break;
+		case GPIOD_EN:
+				RCC->AHB1RSTR |= (1<<3);
+				break;
+		case GPIOE_EN:
+				RCC->AHB1RSTR |= (1<<4);
+				break;
+		case GPIOH_EN:
+				RCC->AHB1RSTR |= (1<<7);
+				break;
+		case CRC_EN:
+				RCC->AHB1RSTR |= (1<<12);
+				break;
+		case DMA1_EN:
+				RCC->AHB1RSTR |= (1<<21);
+				break;
+		case DMA2_EN:
+				RCC->AHB1RSTR |= (1<<22);
+				break;
+		/* AHB2 */
+		case OTGFS_EN:
+				RCC->AHB2RSTR |= (1<<7);
+				break;
+		/* APB1 */
+		case TIM2_EN:
+				RCC->APB1RSTR |= (1<<0);
+				break;
+		case TIM3_EN:
+				RCC->APB1RSTR |= (1<<1);
+				break;
+		case TIM4_EN:
+				RCC->APB1RSTR |= (1<<2);
+				break;
+		case TIM5_EN:
+				RCC->APB1RSTR |= (1<<3);
+				break;
+		case WWDG_EN:
+				RCC->APB1RSTR |= (1<<11);
+				break;
+		case SPI2_EN:
+				RCC->APB1RSTR |= (1<<14);
+				break;
+		case SPI3_EN:
+				RCC->APB1RSTR |= (1<<15);
+				break;
+		case USART2_EN:
+				RCC->APB1RSTR |= (1<<17);
+				break;
+		case I2C1_EN:
+				RCC->APB1RSTR |= (1<<21);
+				break;
+		case I2C2_EN:
+				RCC->APB1RSTR |= (1<<22);
+				break;
+		case I2C3_EN:
+				RCC->APB1RSTR |= (1<<23);
+				break;
+		case PWR_EN:
+				RCC->APB1RSTR |= (1<<28);
+				break;
+		/* APB2 */
+		case TIM1_EN:
+				RCC->APB2RSTR |= (1<<0);
+				break;
+		case USART1_EN:
+				RCC->APB2RSTR |= (1<<4);
+				break;
+		case USART6_EN:
+				RCC->APB2RSTR |= (1<<5);
+				break;
+		case ADC1_EN:
+				RCC->APB2RSTR |= (1<<8);
+				break;
+		case SDIO_EN:
+				RCC->APB2RSTR |= (1<<11);
+				break;
+		case SPI1_EN:
+				RCC->APB2RSTR |= (1<<12);
+				break;
+		case SPI4_EN:
+				RCC->APB2RSTR |= (1<<13);
+				break;
+		case SYSCFG_EN:
+				RCC->APB2RSTR |= (1<<14);
+				break;
+		case TIM9_EN:
+				RCC->APB2RSTR |= (1<<16);
+				break;
+		case TIM10_EN:
+				RCC->APB2RSTR |= (1<<17);
+				break;
+		case TIM11_EN:
+				RCC->APB2RSTR |= (1<<18);
+				break;
+		default:
+			break;
 	}
 }
