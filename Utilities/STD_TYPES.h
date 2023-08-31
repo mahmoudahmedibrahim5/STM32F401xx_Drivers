c4c793557547ea4bae568de673b2b7d3c2d490af
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2023-09-01 01:04:51 +0300

    Update some naming conventions

---
+++ Utilities/STD_TYPES.h
@@ -0,0 +1,28 @@
+/*
+ * DataTypes.h
+ *
+ *  Created on: Feb 8, 2023
+ *      Author: ADMIN
+ */
+
+#ifndef DATATYPES_H_
+#define DATATYPES_H_
+
+#define NULL (void *)0
+
+typedef unsigned char u8;
+typedef signed char s8;
+
+typedef unsigned short int u16;
+typedef signed short int s16;
+
+typedef unsigned long int u32;
+typedef signed long int s32;
+
+typedef unsigned long long int u64;
+typedef signed long long int s64;
+
+typedef float f32;
+typedef double f64;
+
+#endif /* DATATYPES_H_ */
